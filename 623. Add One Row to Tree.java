59b37393883fe1323b721f654fded4b3abafabc8
caomingkai <caomingkai@outlook.com>
2017-06-26 22:41:42 -0700

    BFS DFS

---
+++ 623. Add One Row to Tree.java
@@ -0,0 +1,83 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    // version 1: BFS
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (d == 1) {
+            TreeNode newroot = new TreeNode(v);
+            newroot.left = root;
+            return newroot;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        for (int i = 0; i < d-2; i++) {
+            int size = queue.size();
+            for (int j = 0; j < size; j++) {
+                TreeNode t = queue.poll();
+                if (t.left != null) queue.add(t.left);
+                if (t.right != null) queue.add(t.right);
+            }
+        }
+        while (!queue.isEmpty()) {
+            TreeNode t = queue.poll();
+            TreeNode tmp = t.left;
+            t.left = new TreeNode(v);
+            t.left.left = tmp;
+            tmp = t.right;
+            t.right = new TreeNode(v);
+            t.right.right = tmp;
+        }
+        return root;
+    }
+    
+    /*
+    // version 2:DFS
+    private void dfs(TreeNode root, int depth, int v, int d) {
+        if (root == null) return;
+        if (depth < d-1) {
+            dfs(root.left, depth+1, v, d);
+            dfs(root.right, depth+1,v, d);
+        } else {
+            TreeNode tmp = root.left;
+            root.left = new TreeNode(v);
+            root.left.left = tmp;
+            tmp = root.right;
+            root.right = new TreeNode(v);
+            root.right.right = tmp;
+        }
+    }
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (d == 1) {
+            TreeNode newroot = new TreeNode(v);
+            newroot.left = root;
+            return newroot;
+        }
+        dfs(root, 1, v, d);
+        return root;
+    }
+    */
+    
+    /*
+    // version 3: DFS
+     public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if (d < 2) {
+            TreeNode newroot = new TreeNode(v);
+            if (d == 0) newroot.right = root;
+            else newroot.left = root;
+            return newroot;
+        }
+        if (root == null) return null;
+        root.left = addOneRow(root.left, v, d == 2 ? 1 : d-1);
+        root.right = addOneRow(root.right, v, d == 2 ? 0 : d-1);
+        return root;
+    }
+    */
+}
