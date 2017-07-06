c79d58455a6dd640ab5b0a4fe0573eb4ec244745
caomingkai <caomingkai@outlook.com>
2017-07-05 23:17:34 -0700

    recursion

---
+++ 236. Lowest Common Ancestor of a Binary Tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    // version 1
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+    
+    /*
+    // version 2
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){
+            return null;
+        }
+
+        if(root == p || root == q){
+            return root;
+        }
+
+        TreeNode l = lowestCommonAncestor(root.left,p,q);
+        TreeNode r = lowestCommonAncestor(root.right,p,q);
+
+        if(l != null && r != null){
+            return root;
+        }
+
+        return l != null ? l:r;
+    }
+    */
+}
+
+
