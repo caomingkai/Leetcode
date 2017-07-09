f97f87af0dbc4387ea43f4d4ecaf1e086a9fab8e
caomingkai <caomingkai@outlook.com>
2017-07-08 23:57:30 -0700

    recursive + iterative( BFS & DFS )

---
+++ 104. Maximum Depth of Binary Tree.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    // version 1: recursive
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+    /*
+    // version 2: iterative DFS
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<Integer> value = new Stack<>();
+        stack.push(root);
+        value.push(1);
+        int max = 0;
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            int temp = value.pop();
+            max = Math.max(temp, max);
+            if(node.left != null) {
+                stack.push(node.left);
+                value.push(temp+1);
+            }
+            if(node.right != null) {
+                stack.push(node.right);
+                value.push(temp+1);
+            }
+        }
+        return max;
+    }
+    
+    
+    // version 3: iterative BFS
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int count = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            while(size-- > 0) {
+                TreeNode node = queue.poll();
+                if(node.left != null) {
+                    queue.offer(node.left);
+                }
+                if(node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            count++;
+        }
+        return count;
+    }
+    */
+}
