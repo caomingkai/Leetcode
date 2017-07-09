65df9b4a56a25a9624239b8a4457b737b7032350
Cao Mingkai <caomingkai@outlook.com>
2017-07-08 22:53:00 -0700

    Update 236. Lowest Common Ancestor of a Binary Tree.java

--- 236. Lowest Common Ancestor of a Binary Tree.java
+++ 236. Lowest Common Ancestor of a Binary Tree.java
@@ -9,34 +9,25 @@
  */
 public class Solution {
     // version 1
+    // main idea:
+    // -- see if a Node is ancestor of p or q
+    // ----- if YES, return p/q (to prove it)
+    // ----- if NO , return null
+    // -- if a Node has left child ( ancestor of p )
+    // -- meanwhile,has right child( ancestro of q )
+    // -- we could say it is the shortest ancestor
+    // -- if a Node only has left child( or right child ) as ancestor, then we have to wait to see the status of its right child
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) return root;
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        return left == null ? right : right == null ? left : root;
+        if(left != null && right != null)   return root;
+        // the line below has many functionalities:
+        // 1 - if left and right has one null, return non-null
+        // 2 - if left and right are both null, return null
+        return left != null ? left : right;
     }
     
-    /*
-    // version 2
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null){
-            return null;
-        }
-
-        if(root == p || root == q){
-            return root;
-        }
-
-        TreeNode l = lowestCommonAncestor(root.left,p,q);
-        TreeNode r = lowestCommonAncestor(root.right,p,q);
-
-        if(l != null && r != null){
-            return root;
-        }
-
-        return l != null ? l:r;
-    }
-    */
 }
 
 
