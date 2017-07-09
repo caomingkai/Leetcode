5a61c0e41831fdf52da3a3a53557d934f3687488
Cao Mingkai <caomingkai@outlook.com>
2017-07-08 23:53:18 -0700

    Update 236. Lowest Common Ancestor of a Binary Tree.java

--- 236. Lowest Common Ancestor of a Binary Tree.java
+++ 236. Lowest Common Ancestor of a Binary Tree.java
@@ -7,6 +7,9 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// version_3  vs version_4 => why version_4 is not good ?
+
+// version 1: recursion
 public class Solution {
     // version 1
     // main idea:
@@ -62,4 +65,116 @@ public class Solution {
         return q;
     }
 }
+
+// version 3: How to find path from Root to target Node ???
+// return type: boolean  --> only store Node with true return value in Stack
+// use parameter var to store Nodes
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Stack<TreeNode> pStack = new Stack<TreeNode>();
+        Stack<TreeNode> qStack = new Stack<TreeNode>();
+        TreeNode target = null;
+        if (findPath(root, p, pStack) && findPath(root, q, qStack)) {
+            while (!pStack.isEmpty()) {
+                TreeNode pNode = pStack.pop();
+                if (qStack.contains(pNode))
+                    target = pNode;
+            }
+        } 
+        return target;
+    }
+    private boolean findPath(TreeNode root, TreeNode node, Stack<TreeNode> stack) {
+        if (root == null)
+            return false;
+        if (root == node) {
+            stack.push(root);
+            return true;
+        } else {
+            if (findPath(root.left, node, stack) ||  findPath(root.right, node, stack)) {
+                stack.push(root);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+// version 4:
+// return type: void
+// use global var to store Nodes
+// since when pass Class as parameter variable, it is passed in address, not copy!!!
+// this version is intended to use it as copy, so it has to manually copy
+// version_3 is intended to pass address, like a global var, so it can save a lot space.
+public class Solution {
+    
+    boolean flag = true;                         // indicate whether we should continue traverse
+    List<TreeNode> pathP = new ArrayList<>();    // store path from root to p
+    List<TreeNode> pathQ = new ArrayList<>();    // store path from root to q
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        inorderP( root, p, pathP );  // find out the path to p
+        flag = true;                 // initialize flag for next call
+        inorderQ( root, q, pathQ );  // find out the path to q
+
+
+        int l = pathP.size() < pathQ.size() ? pathP.size() : pathQ.size();
+        int i = 0;
+        while( i < l && pathP.get(i) == pathQ.get(i) ){  // find out the 1st pair of different nodes fromo pathQ and pathP
+            i++;
+        }
+        return pathP.get(i-1);
+    }
+    
+    private void inorderP( TreeNode root, TreeNode t, List<TreeNode> path ){
+        // have to copy the parameter path to p.
+        // otherwise, the 1st call in the following two calls of inorder() would change path , leading to 2nd call fail.
+        // applying copy, we can make sure that path would never be modified for both two calls
+        List<TreeNode> p = new ArrayList<>();
+        for( int i = 0; i < path.size(); i++ ){
+            p.add( path.get(i));
+        }
+
+        if( flag == true){
+            if( root == null ) return;
+            if( root == t ){
+                 p.add(root);
+                for( int i = 0; i < p.size(); i++ ){
+                    pathP.add( p.get(i));
+                }
+                flag = false;
+                return;
+            }else{
+                p.add(root);
+                inorderP( root.left,  t, p );
+                inorderP( root.right, t, p );
+            }
+        }
+    }
+
+    private void inorderQ( TreeNode root, TreeNode t, List<TreeNode> path ){
+        List<TreeNode> p = new ArrayList<>();
+        for( int i = 0; i < path.size(); i++ ){
+            p.add( path.get(i));
+        }
+
+        if( flag == true){
+            if( root == null ) return;
+            if( root == t ){
+                p.add(root);
+                for( int i = 0; i < p.size(); i++ ){
+                    pathQ.add( p.get(i));
+                }
+                flag = false;
+                return;
+            }else{
+                p.add(root);
+                inorderQ( root.left,  t, p );
+                inorderQ( root.right, t, p );
+            }
+        }
+    }
+}
+
+
 
