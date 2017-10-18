b970e2658e1458ff94cb9f853fc4c89327f92f6f
caomingkai <caomingkai@outlook.com>
2017-10-17 20:30:58 -0700

    heap + set

---
+++ 359. Logger Rate Limiter.java
@@ -0,0 +1,80 @@
+class Logger {
+
+    /** Initialize your data structure here. */
+    HashMap<String, Integer> map;
+    public Logger() {
+        map = new HashMap<>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if( !map.containsKey(message) || timestamp - map.get( message) >= 10  ){
+            map.put( message, timestamp );
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+
+class Log {
+    int timestamp;
+    String message;
+    public Log(int aTimestamp, String aMessage) {
+        timestamp = aTimestamp;
+        message = aMessage;
+    }
+}
+
+/*
+// version 2: only keep recent 10 min messages, using heap
+public class Logger {
+    PriorityQueue<Log> recentLogs;
+    Set<String> recentMessages;   
+    
+    /** Initialize your data structure here. */
+    public Logger() {
+        recentLogs = new PriorityQueue<Log>(10, new Comparator<Log>() {
+            public int compare(Log l1, Log l2) {
+                return l1.timestamp - l2.timestamp;
+            }
+        });
+        
+        recentMessages = new HashSet<String>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        while (recentLogs.size() > 0)   {
+            Log log = recentLogs.peek();
+            // discard the logs older than 10 minutes
+            if (timestamp - log.timestamp >= 10) {
+                recentLogs.poll();
+                recentMessages.remove(log.message);
+            } else 
+            	break;
+        }
+        boolean res = !recentMessages.contains(message);
+        if (res) {
+            recentLogs.add(new Log(timestamp, message));
+            recentMessages.add(message);
+        }
+        return res;
+    }
+}
+*/
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
