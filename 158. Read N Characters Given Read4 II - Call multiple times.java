e7bba1578b2bbecdfd29e624947169adf50f601b
caomingkai <caomingkai@outlook.com>
2017-10-20 12:45:00 -0700

    read4()

---
+++ 158. Read N Characters Given Read4 II - Call multiple times.java
@@ -0,0 +1,31 @@
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    char[] prevBuf = new char[4];
+    int prevSize = 0;
+    int prevIndex = 0;
+    
+    public int read(char[] buf, int n) {
+        int counter = 0;
+        
+        while (counter < n) {
+            if (prevIndex < prevSize) {
+                buf[counter++] = prevBuf[prevIndex++];
+            } else {
+                prevSize = read4(prevBuf);
+                prevIndex = 0;
+                if (prevSize == 0) {
+                    // no more data to consume from stream
+                    break;
+                }
+            }
+        }
+        return counter;
+    }
+}
