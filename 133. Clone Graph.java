ce70e21a828ac2bcb973f7dcecfb88b59e50655c
caomingkai <caomingkai@outlook.com>
2017-12-26 20:37:03 -0800

    graph traversal

---
+++ 133. Clone Graph.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    
+    Map<Integer, UndirectedGraphNode> map = new HashMap<>();
+        
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        
+        if( node == null )
+            return null;
+        
+        if( map.containsKey(node.label) )
+            return map.get(node.label);
+        
+        UndirectedGraphNode newNode = new UndirectedGraphNode( node.label );
+        map.put(newNode.label, newNode);
+        for( UndirectedGraphNode element: node.neighbors ){
+            UndirectedGraphNode neighbor = cloneGraph( element );
+            newNode.neighbors.add( neighbor );
+        }
+        
+        return newNode;
+    }
+}
