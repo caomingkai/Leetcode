ef2e90ee05c3f2889c66dd4d3374e34eea1a89e3
caomingkai <caomingkai@outlook.com>
2017-12-27 11:56:45 -0800

    reverse linkedlist

--- 206. Reverse Linked List.java
+++ 206. Reverse Linked List.java
@@ -6,16 +6,47 @@
  *     ListNode(int x) { val = x; }
  * }
  */
-public class Solution {
+
+
+
+// version 1: good version, in-place
+class Solution {
     public ListNode reverseList(ListNode head) {
-        if( head == null ) return head;
-        ListNode tail = head; // after reversion, the head would be the tail, and tail pointer wouldn't change in process
-        while( tail.next != null ){
-            ListNode nextE = tail.next;
-            tail.next = nextE.next;
-            nextE.next = head;
-            head = nextE;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
         }
-        return head;
+        return prev;
     }
 }
+
+/*
+// version 2: bad version, due to O(n) space complexity
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        if( head == null )
+            return null;
+        
+        
+        ListNode ptr1 = head;
+        ListNode ptr2 = null;
+        
+        while( ptr1 != null ){
+            ListNode shadow = new ListNode( ptr1.val );
+            shadow.next = ptr2;
+            ptr1 = ptr1.next;
+            ptr2 = shadow;
+        }
+        head = ptr2;
+        
+        
+        return head;
+        
+    }
+}
+*/
