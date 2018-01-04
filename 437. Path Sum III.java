12b2acbd50e7b00dd5f0e20defe015aa33ecf856
caomingkai <caomingkai@outlook.com>
2018-01-03 18:41:45 -0800

    backtrack + great recursion

--- 437. Path Sum III.java
+++ 437. Path Sum III.java
@@ -7,44 +7,57 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
-    
-    public int pathSum(TreeNode root, int sum) {
-        HashMap<Integer, Integer> preSum = new HashMap();
-        preSum.put(0,1);
-        return helper(root, 0, sum, preSum);
-    }
-    
-    public int helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
-        if (root == null) {
-            return 0;
-        }
-        
-        currSum += root.val;
-        int res = preSum.getOrDefault(currSum - target, 0);
-        preSum.put(currSum, preSum.getOrDefault(currSum, 0) + 1);
-        
-        res += helper(root.left, currSum, target, preSum) + helper(root.right, currSum, target, preSum);
-        preSum.put(currSum, preSum.get(currSum) - 1);
-        return res;
-    }
-}
-
 
 /*
-// version 2:
-// Space: O(n) due to recursion.
-// Time: O(n^2) in worst case (no branching); O(nlogn) in best case (balanced tree).
-public class Solution {
+// version 1: my backtrack
+class Solution {
+    
+    int res = 0;
+    
     public int pathSum(TreeNode root, int sum) {
-        if (root == null) return 0;
-        return pathSumFrom(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0,1);
+        
+        helper(root, sum, 0, map);
+        return res;
     }
     
-    private int pathSumFrom(TreeNode node, int sum) {
-        if (node == null) return 0;
-        return (node.val == sum ? 1 : 0) 
-            + pathSumFrom(node.left, sum - node.val) + pathSumFrom(node.right, sum - node.val);
+    private void helper( TreeNode root, int target, int curSum, HashMap<Integer, Integer> map ){
+        if( root == null )
+            return;
+        
+        int newSum = root.val + curSum;
+        if( map.containsKey( newSum-target ) ){
+            res += map.get( newSum-target );
+        }
+        map.put( newSum, map.getOrDefault(newSum, 0) + 1 );
+        
+        if( root.left != null ) helper(root.left, target, newSum, map);
+        if( root.right != null ) helper(root.right, target, newSum, map);
+        
+        map.put(newSum, map.get(newSum) - 1);
     }
+     
 }
 */
+
+// version 2: 
+public class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null)
+            return 0;
+        return findPath(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    }
+    
+    public int findPath(TreeNode root, int sum){
+        int res = 0;
+        if(root == null)
+            return res;
+        if(sum == root.val)
+            res++;
+        res += findPath(root.left, sum - root.val);
+        res += findPath(root.right, sum - root.val);
+        return res;
+    }
+   
+}
