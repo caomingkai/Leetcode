921a7ccfd85049e4a78b8e6579964aa5c9de2ff7
caomingkai <caomingkai@outlook.com>
2018-01-13 14:20:19 -0800

    char[] default value is 0

--- 205. Isomorphic Strings.java
+++ 205. Isomorphic Strings.java
@@ -1,49 +1,80 @@
-// version 1: bucket hash for char
-public class Solution {
+class Solution {
+    
+    // KEY IDEA: 
+    // 1 : two character in s don't point to same character in t
+    // 2 : two character in t don't point to same character in s
+    
+    /*
+    // version 1:  two pass : one in-order, one reverse-order
     public boolean isIsomorphic(String s, String t) {
-        int[] map = new int[256];    // actually store two arrays in two piece of 256 memory
-        
-        for (int i = 0; i < s.length(); i++) {
-            int sIndex = s.charAt(i);
-            int tIndex = t.charAt(i) + 128;
-            
-            if (map[sIndex] != map[tIndex]) {
-                return false;
-            }
-            
-            map[sIndex] = i + 1;  // since all elements map is initialized to '0'
-            map[tIndex] = i + 1; 
-        }
-        
-        return true;
+        return helper(s,t) && helper(t,s);
     }
-}
-/*
-// version 2: hashmap
-public class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        if( s.length() == 0 ) return true;
+    
+    private boolean helper( String s, String t ){
+        if( s == null || s.length() == 0) return true;
         
+        HashMap<Character, Character> map = new HashMap<>();
         int l = s.length();
-        
-        Map<Character, Character> maps = new HashMap<>();
-        Map<Character, Character> mapt = new HashMap<>();
-        
-        for( int i = 0; i < l; i++){
-            if( maps.containsKey( s.charAt(i)) ){
-                if( t.charAt(i) != maps.get( s.charAt(i) ) ) return false;
-            }else{
-                maps.put( s.charAt(i), t.charAt(i) );
-            }
+        for( int i = 0; i < l; i++ ){
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
             
-            if( mapt.containsKey( t.charAt(i)) ){
-                if( s.charAt(i) != mapt.get( t.charAt(i) ) ) return false;
-            }else{
-                mapt.put( t.charAt(i), s.charAt(i) );
-            }
-            
+            if( map.containsKey(sc) ){
+                char tc_visited = map.get(sc);
+                if( tc_visited != tc )
+                    return false;
+            }else
+                map.put( sc, tc );
         }
+        
         return true;
     }
+    */
+    
+    /*
+    // version 2: hashMap + hashSet(used?)
+    public boolean isIsomorphic(String s, String t) {
+        if( s == null || s.length() == 0) return true;
+        HashSet<Character> set = new HashSet<>();
+        HashMap<Character, Character> map = new HashMap<>();
+        int l = s.length();
+        for( int i = 0; i < l; i++ ){
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+            if( map.containsKey(sc) ){
+                char tc_visited = map.get(sc);
+                if( tc_visited != tc )
+                    return false;
+            }else{
+                if(set.contains(tc))
+                    return false;
+                set.add(tc);
+                map.put( sc, tc );
+            }
+        }
+        
+        return true;
+    }
+    */
+    
+    // version 3: char array, default char is equavilent to 0
+    public boolean isIsomorphic(String s, String t) {
+	        char[] map = new char[256];
+	        boolean[] used = new boolean[256];
+	        char[] sc = s.toCharArray();
+	        char[] tc = t.toCharArray();
+	        for (int i = 0; i < sc.length; i++) {
+	            if (map[sc[i]] == 0) {   // char array default value
+	                if (used[tc[i]]) {   // 1.ensure: two character in s don't point to same character in t
+	                	return false;	
+	                }
+	                map[sc[i]] = tc[i];  
+	                used[tc[i]] = true;
+	            } else {
+	                if (map[sc[i]] != tc[i]) return false; // 2.ensure: two character in t don't point to same character in s
+	            }
+	        }
+	        return true;
+    }
+    
 }
-*/
