d6efa6f19f8f661abe56aa6336aedb2d55a7710f
caomingkai <caomingkai@outlook.com>
2018-01-15 14:58:56 -0800

    hashSet avoid duplicate value

--- 47. Permutations II.java
+++ 47. Permutations II.java
@@ -1,41 +1,67 @@
+/*
+1 
+2
+
+1 1
+1 2
+2 1
+
+1 1 2
+1 2 1
+2 1 1
+
+maintain a HashMap contains the items left needs to be inserted(only nums>0, we could use it)
+when  prefix.size() == max, add to res
+*/
+/*
+
+[1 1 2 3]
+
+1 
+2
+3
+
+1 1
+1 2
+1 3
+
+2 1
+2 3
+3 1
+3 2
+
+*/
+
 class Solution {
+
+    public List<List<Integer>> permuteUnique( int[] nums ){
     
-    List<List<Integer>> res = new ArrayList<>();
-    
-    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if( nums == null || nums.length == 0 ) 
+            return res;
         
-        int l = nums.length;
-        if( l == 0 ) return null;
-        
-        HashMap<Integer, Integer> hash = new HashMap<>();
-        for( int i = 0; i < l; i++ ){
-            hash.put(nums[i], hash.getOrDefault(nums[i],0)+1 );
-        }
-        
-        Set<Integer> set = hash.keySet();
-        ArrayList<Integer> prefix = new ArrayList<Integer>();
-        backtrack( hash, set, prefix, l );
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for( int item: nums) map.put( item, map.getOrDefault(item,0) + 1 );
+        List<Integer> prefix = new ArrayList<>();
+        backtrack( prefix, map, res, nums.length );
         return res;
     }
     
-    private void backtrack( HashMap<Integer, Integer> hash, Set<Integer>set, ArrayList<Integer> prefix ,int l ){
-        //edge case:
-        if( prefix.size() == l ){
-            res.add(new ArrayList<>(prefix));
-            return;
-        }
-        
-        // recursive case:
-        Iterator<Integer> iter = set.iterator();
-        while( iter.hasNext() ){
-            Integer digit = iter.next();
-            if( hash.get(digit) > 0 ){
-                prefix.add(digit);
-                hash.put( digit, hash.get(digit)-1 );
-                backtrack( hash, set, prefix, l );
-                prefix.remove(prefix.size() - 1);
-                hash.put( digit, hash.get(digit)+1 );
+    private void backtrack( List<Integer> prefix, HashMap<Integer, Integer> map, List<List<Integer>> res, int max){
+    
+        if( prefix.size() == max )
+            res.add( new ArrayList(prefix) );
+        else{
+            for( int key: map.keySet() ) {
+                if( map.get(key) > 0 ){
+                    prefix.add( key );
+                    map.put(key, map.get(key)-1);
+                    backtrack( prefix, map, res, max);
+                    map.put(key, map.get(key)+1);
+                    prefix.remove( prefix.size() - 1 );
+                }
             }
+            
         }
     }
 }
