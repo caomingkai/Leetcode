6318e08b7d68a6cd0be65e6e263e9ab1353e3a3a
caomingkai <caomingkai@outlook.com>
2018-01-17 00:01:50 -0800

    fast-slow pointers

--- 142. Linked List Cycle II.java
+++ 142. Linked List Cycle II.java
@@ -9,6 +9,24 @@
  *     }
  * }
  */
+
+
+/*
+ -0- 用slower和faster方法判断是否有环；
+     设链表的头节点是head，环的入口节点是entry，slower和faster2个指针相遇的节点是meeting;
+     L1是head到entry的正向距离
+     L2是entry到meeting的正向距离，
+     C是环的长度，
+     n是faster指针在cycle里遍历的次数(不到一遍算0)
+     
+ -1- 当slower和faster相遇时，slower已经走了L1 + L2的距离，也即head和meeting的距离;
+ -2- 当slower和faster相遇时，faster已经走了L1 + L2 + C的距离;
+ -3- 因为slower步进1，而faster步进2，那么当slower和faster第一次相遇时，faster已走的距离是slower已经走的距离的两倍，即
+     2* (L1 + L2) = L1 + L2 +  C    ===>   L1 = C - L2
+ -4- L1 = C - L2 这个等式表明， head和entry的距离(L1)，等于meeting到entry的正向距离（链表是有遍历方向的）。
+
+ -5- ====>  L1 = C - L2
+*/
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         
