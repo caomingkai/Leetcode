04f99fc6c3e344752157e1a4b31cfb059b0d54b8
caomingkai <caomingkai@outlook.com>
2018-01-18 18:55:14 -0800

    hashset/ hashMap -> silding window

--- 3. Longest Substring Without Repeating Characters.java
+++ 3. Longest Substring Without Repeating Characters.java
@@ -1,15 +1,44 @@
-class Solution {
+
+public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length()==0) return 0;
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        int max=0;
-        for (int i=0, j=0; i<s.length(); ++i){
-            if (map.containsKey(s.charAt(i))){
-                j = Math.max(j,map.get(s.charAt(i))+1);
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i); // ptr i always point to the first letter
+                                                       //    a b c d a b c a b
+                                                       //    |       |     | 
             }
-            map.put(s.charAt(i),i);
-            max = Math.max(max,i-j+1);
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
         }
-        return max;
+        return ans;
     }
 }
+
+/*
+
+// version 2: HashSet
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j));
+                j++;
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return ans;
+    }
+}
+
+*/
