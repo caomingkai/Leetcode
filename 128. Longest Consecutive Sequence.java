44597c72f56da43fbaa315176358605df32e4427
caomingkai <caomingkai@outlook.com>
2018-01-27 23:35:10 -0800

    avoid duplicate using set()

--- 128. Longest Consecutive Sequence.java
+++ 128. Longest Consecutive Sequence.java
@@ -1,3 +1,121 @@
+
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        // edge case
+        if( nums == null || nums.length == 0 ) return 0;
+        
+        HashSet<Integer> set = new HashSet<>();
+        for( int i : nums ) set.add(i);
+        int res = 0;
+        
+        for( Integer item : set ){
+            if( set.contains(item-1) )  //只要有比该元素还小的元素，我们就跳过该元素，直到遇到
+                continue;               //直到这个consecutive list的首元素，再统计长度；以防止重复统计。
+            else{
+                int sum = 1;            //没有我该元素小的，表明这是首元素，统计长度
+                int next = item+1;
+                while( set.contains(next) ){
+                    sum++;
+                    next++;
+                }
+                res = Math.max( res, sum );
+            }
+        }
+        
+        return res;
+    }
+}
+
+/*
+// version 1: union-find
+
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        // edge case
+        if( nums == null || nums.length == 0 ) return 0;
+        
+        // general case
+        int len = nums.length;
+        UF unionFind = new UF( len );
+        
+        int res = 1;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for( int i = 0; i < len; i++ ) {
+            if( !hm.containsKey ( nums[i] ) ){
+                hm.put( nums[i], i );
+            }
+        }
+        
+        
+        for( Integer item : hm.keySet() ){
+            if( hm.containsKey( item-1 ) )
+                unionFind.union( hm.get(item), hm.get(item-1) );
+            
+            if( hm.containsKey( item+1 ) )
+                unionFind.union( hm.get(item), hm.get(item+1) );
+        }
+        
+        return unionFind.maxUnionNum();
+    }
+    
+    
+    class UF{
+        private int[] parent;
+        private int[] weight;
+        private int n;
+        
+        public UF( int num ){
+            n = num;
+            weight = new int[n];
+            parent = new int[n];
+            for( int i = 0; i < n; i++ )
+                parent[i] = i;
+        }
+        
+        public int find( int i ){
+            while( i != parent[i] ){
+                parent[i] = parent[ parent[i] ];
+                i = parent[i];
+            }
+            return i;
+        }
+        
+        public void union( int i, int j ){
+            int iRoot = find(i);
+            int jRoot = find(j);
+            if( iRoot == jRoot ) return;
+            
+            int iWt = weight[i];
+            int jWt = weight[j];
+            if( iWt < jWt ){
+                parent[iRoot] = jRoot;
+                weight[jRoot] += weight[iRoot];
+            }else{
+                parent[jRoot] = iRoot;
+                weight[iRoot] += weight[jRoot];
+            }
+        }
+        
+        public int maxUnionNum(){
+            int res = 1;
+            int[] cnt = new int[n];
+            for( int i = 0; i < n; i++ ){
+                int root = find(i) ;
+                cnt[root]++;
+                res = Math.max( res, cnt[root] );
+            }
+            return res;
+        }
+        
+    }
+}
+
+*/
+
+/*
+// version 2: start counting from smallest item of each sequences, with help of Set ( ignoring current item if exist item-1 )
 class Solution {
     public int longestConsecutive(int[] nums) {
         
@@ -28,3 +146,4 @@ class Solution {
         return res;
     }
 }
+*/
