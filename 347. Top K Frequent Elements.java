319edd780c4f26820ba0c3fbc361341f7265cf0b
caomingkai <caomingkai@outlook.com>
2018-01-30 16:15:22 -0800

    bucket sort & heap

--- 347. Top K Frequent Elements.java
+++ 347. Top K Frequent Elements.java
@@ -1,3 +1,7 @@
+
+/*
+
+// version 1: hashMap + heap
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
         
@@ -9,20 +13,59 @@ class Solution {
         
         
         // 2. put them into into a size k min_heap( priorityQueue ),  output reverse order
-        PriorityQueue< Map.Entry<Integer,Integer> > pq = new PriorityQueue< Map.Entry<Integer,Integer> >( new Comparator< Map.Entry<Integer,Integer> > (){
-            @Override
-            public int compare( Map.Entry<Integer,Integer> m1, Map.Entry<Integer,Integer> m2 ){
-                return m2.getValue() - m1.getValue();
-            }
-        } );
+//         PriorityQueue< Map.Entry<Integer,Integer> > pq = new PriorityQueue< Map.Entry<Integer,Integer> >( new Comparator< Map.Entry<Integer,Integer> > (){
+//             @Override
+//             public int compare( Map.Entry<Integer,Integer> m1, Map.Entry<Integer,Integer> m2 ){
+//                 return m2.getValue() - m1.getValue();
+//             }
+//         } );
         
-        pq.addAll( map.entrySet() );
+//         pq.addAll( map.entrySet() );
         
-        List<Integer> res = new ArrayList<>(k);
-        for( int i = 0; i < k; i++ ){
-            res.add(pq.poll().getKey());
+        PriorityQueue< Integer > pq = new PriorityQueue<>(
+                                           (Integer n1, Integer n2)->map.get(n1) - map.get(n2));
+        
+        for( Map.Entry<Integer,Integer> en : map.entrySet() ){
+            pq.offer( en.getKey() );
+            if( pq.size() > k )
+                pq.poll();
         }
         
+        List<Integer> res = new ArrayList<>(k);
+        for( int i = k-1; i >=0; i-- ){
+            res.add(pq.poll());
+        }
+        
+        return res;
+    }
+}
+*/
+// version 2: bucket sort
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+        List<Integer>[] bucket = new List[nums.length + 1];
+        Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+
+        for (int n : nums) {
+            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+        }
+
+        for (int key : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+            if (bucket[pos] != null) {
+                res.addAll(bucket[pos]);
+            }
+        }
         return res;
     }
 }
