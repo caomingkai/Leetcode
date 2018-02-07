b182aaa1cadba786900ef92f8b65a51641ec2dfb
caomingkai <caomingkai@outlook.com>
2018-02-06 22:43:57 -0800

    10s time window :

--- 359. Logger Rate Limiter.java
+++ 359. Logger Rate Limiter.java
@@ -1,54 +1,49 @@
 class Logger {
 
-    /** Initialize your data structure here. */
-    HashMap<String, Integer> map;
+    /*
+    
+    HashMap<String, Integer> cache = new HashMap<>();
     public Logger() {
-        map = new HashMap<>();
+        
     }
     
-    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity. */
     public boolean shouldPrintMessage(int timestamp, String message) {
-        if( !map.containsKey(message) || timestamp - map.get( message) >= 10  ){
-            map.put( message, timestamp );
-            return true;
-        }else{
-            return false;
+        if( cache.containsKey(message) ){
+            int t = cache.get(message);
+            if( timestamp-t < 10 )
+                return false;
+            else{
+                cache.put( message, timestamp );
+                return true;
+            }
+        }
+        cache.put( message, timestamp );
+        return true;
+    }
+    */
+    class Log {
+        int timestamp;
+        String message;
+        public Log(int aTimestamp, String aMessage) {
+            timestamp = aTimestamp;
+            message = aMessage;
         }
     }
-}
 
-
-class Log {
-    int timestamp;
-    String message;
-    public Log(int aTimestamp, String aMessage) {
-        timestamp = aTimestamp;
-        message = aMessage;
-    }
-}
-
-/*
-// version 2: only keep recent 10 min messages, using heap
-public class Logger {
     PriorityQueue<Log> recentLogs;
     Set<String> recentMessages;   
-    
-    /** Initialize your data structure here. */
+
+        /** Initialize your data structure here. */
     public Logger() {
         recentLogs = new PriorityQueue<Log>(10, new Comparator<Log>() {
             public int compare(Log l1, Log l2) {
                 return l1.timestamp - l2.timestamp;
             }
         });
-        
         recentMessages = new HashSet<String>();
     }
-    
-    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity. */
+
+
     public boolean shouldPrintMessage(int timestamp, String message) {
         while (recentLogs.size() > 0)   {
             Log log = recentLogs.peek();
@@ -57,7 +52,7 @@ public class Logger {
                 recentLogs.poll();
                 recentMessages.remove(log.message);
             } else 
-            	break;
+                break;
         }
         boolean res = !recentMessages.contains(message);
         if (res) {
@@ -66,15 +61,5 @@ public class Logger {
         }
         return res;
     }
+
 }
-*/
-/**
- * Your Logger object will be instantiated and called as such:
- * Logger obj = new Logger();
- * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
- */
-/**
- * Your Logger object will be instantiated and called as such:
- * Logger obj = new Logger();
- * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
- */
