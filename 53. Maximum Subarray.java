0d91576babe1a0cb3a347eed4d6ccaa8585beddc
caomingkai <caomingkai@outlook.com>
2018-02-08 12:37:37 -0800

    D & C

--- 53. Maximum Subarray.java
+++ 53. Maximum Subarray.java
@@ -30,6 +30,7 @@ class Solution {
     */
      
     
+    /*
     //version 2: DP --> TIME: O(n)   SPACE:O(n) 
     // dp[i] means the largest sum among the subarrays whose last element is A[i]
     // 
@@ -45,7 +46,7 @@ class Solution {
         }
         return max;
     }
-    
+    */
     
     /*
     // Version 3:DP ---> TIME: O(n)   SPACE:O(1) 
@@ -59,4 +60,46 @@ class Solution {
     } 
     */
     
+    
+    
+    // version 4: Divide & Conquer
+    public int maxSubArray(int[] nums) {
+        return Subarray(nums, 0 ,nums.length -1 );
+    }
+    public int Subarray(int[] A,int left, int right){
+        if(left == right){return A[left];}
+        int mid = left + (right - left) / 2;
+        int leftSum = Subarray(A,left,mid);// left part 
+        int rightSum = Subarray(A,mid+1,right);//right part
+        int crossSum = crossSubarray(A,left,right);// cross part
+        if(leftSum >= rightSum && leftSum >= crossSum){// left part is max
+            return leftSum;
+        }
+        if(rightSum >= leftSum && rightSum >= crossSum){// right part is max
+            return rightSum;
+        }
+        return crossSum; // cross part is max
+    }
+    public int crossSubarray(int[] A,int left,int right){
+        int leftSum = Integer.MIN_VALUE;
+        int rightSum = Integer.MIN_VALUE;
+        int sum = 0;
+        int mid = left + (right - left) / 2;
+        for(int i = mid; i >= left ; i--){
+            sum = sum + A[i];
+            if(leftSum < sum){
+                leftSum = sum;
+            }
+        }
+        sum = 0;
+        for(int j = mid + 1; j <= right; j++){
+            sum = sum + A[j];
+            if(rightSum < sum){
+                rightSum = sum;
+            }
+        }
+        return leftSum + rightSum;
+    }
 }
+
+
