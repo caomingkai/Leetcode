6fc8e28713edad500459a59fd61caea644a91e32
caomingkai <caomingkai@outlook.com>
2018-02-13 13:21:26 -0800

    考虑linkedlist 带cycle的情况

--- 206. Reverse Linked List.java
+++ 206. Reverse Linked List.java
@@ -29,16 +29,28 @@ class Solution {
     }
     */
 
-    
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        
+        while(cur!=null){
+            ListNode next = cur.next; 
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
   
     // 2- recursion version 
+    /*
     public ListNode reverseList(ListNode head) {
         if (head == null)
             return null;
         
         return h ( null, head);
     }
-    
+    */
     /*                   1 -> 2 -> 3 -> 4
             1st: null <- 1  + h(1, 2)
             2nd  null <- 1 <- 2 + h(2, 3)
@@ -50,6 +62,8 @@ class Solution {
                 - 更新prev、curr
             3. 直到curr的值是null，返回prev
     */
+
+    /*
     private ListNode h(ListNode prev, ListNode curr){
         if( curr == null ){
             return prev;
@@ -59,4 +73,30 @@ class Solution {
         curr.next = prev;
         return h( curr, next );
     }
+    */
+
+
+
+    // version 3: reverse linkedlist with cycle
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head == head.next)
+            return head;
+        
+        ListNode prev = head;
+        ListNode curr = head.next;
+        ListNode anchor = head.next; // 必须设置一个anchor，
+                                     // 不能直接使用head.next作为while结束条件
+                                     // 因为head.next的指向改变了
+
+        do{
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }while( curr != anchor );   // 不能直接使用head.next作为while结束条件！
+
+        return prev.next; // 因为pre最终指向的head是原来的最小值
+                          // 改变指向后，prev[head] next指向的是最大值
+    }
+
 }
