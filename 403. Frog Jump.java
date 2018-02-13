90627a1aaf780f2ffceab66d091c0182edcdb884
caomingkai <caomingkai@outlook.com>
2018-02-12 21:32:28 -0800

    recursion + DP

---
+++ 403. Frog Jump.java
@@ -0,0 +1,54 @@
+
+/*
+
+// version 1: 从当前位置 ind，当前jumpsize 能否到达最后一块石头
+
+public class Solution {
+    public boolean canCross(int[] stones) {
+        int[][] memo = new int[stones.length][stones.length];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        return can_Cross(stones, 0, 0, memo) == 1;
+    }
+    public int can_Cross(int[] stones, int ind, int jumpsize, int[][] memo) {
+        if (memo[ind][jumpsize] >= 0) {
+            return memo[ind][jumpsize];
+        }
+        for (int i = ind + 1; i < stones.length; i++) {
+            int gap = stones[i] - stones[ind];
+            if (gap >= jumpsize - 1 && gap <= jumpsize + 1) {
+                if (can_Cross(stones, i, gap, memo) == 1) {
+                    memo[ind][gap] = 1;
+                    return 1;
+                }
+            }
+        }
+        memo[ind][jumpsize] = (ind == stones.length - 1) ? 1 : 0;
+        return memo[ind][jumpsize];
+    }
+}
+
+*/
+
+// DP
+// 这里不是dp[][]， 而是 <key, val> pair， key: 当前石头pos, val:上块石头pos跳过来所用的size
+public class Solution {
+    public boolean canCross(int[] stones) {
+        HashMap<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < stones.length; i++) {
+            map.put(stones[i], new HashSet<Integer>());
+        }
+        map.get(0).add(0);
+        for (int i = 0; i < stones.length; i++) {
+            for (int k : map.get(stones[i])) {
+                for (int step = k - 1; step <= k + 1; step++) {
+                    if (step > 0 && map.containsKey(stones[i] + step)) {
+                        map.get(stones[i] + step).add(step);
+                    }
+                }
+            }
+        }
+        return map.get(stones[stones.length - 1]).size() > 0;
+    }
+}
