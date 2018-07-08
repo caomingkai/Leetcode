16297f02cd676cbf824540bd7301a411bcd40364
caomingkai <caomingkai@outlook.com>
2018-07-07 18:09:02 -0700

    binary search: r&l -> m -> while -> return

--- 162. Find Peak Element.java
+++ 162. Find Peak Element.java
@@ -1,30 +1,27 @@
-public int findPeakElement(int[] num) {    
-    return helper(num,0,num.length-1);
-}
-
-public int helper(int[] num,int start,int end){
-    if(start == end){
-        return start;
-    }else if(start+1 == end){
-        if(num[start] > num[end]) return start;
-        return end;
-    }else{
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int l = 0;
+        int r = n-1;
         
-        int m = (start+end)/2;
-        
-        if(num[m] > num[m-1] && num[m] > num[m+1]){
-
-            return m;
-
-        }else if(num[m-1] > num[m] && num[m] > num[m+1]){
-
-            return helper(num,start,m-1);
-
-        }else{
-
-            return helper(num,m+1,end);
-
+        /*   1.  先确定左右l、r 的取值情况, 决定 m的计算方式、以及while循环结束条件
+             2.  根据l、r取值情况，决定m往大里取值or往小里取值。
+                 r、l中任何一个若更新为m，那么它有可能导致“死循环”，需要在下次计算m的时候，避免 m 再次落到它上面，即
+                   - 若 l=m, r=m-1, 则 m=(r-l)/2 + l + 1
+                   - 若 l=m+1, r=m, 则 m=(r-l)/2 + l
+             3.  根据l、r取值情况，确定循环结束条件
+                   - 若l、r有任何一个为：l=m 或者 r=m，那么终结条件为最后还剩2个元素： while(l<r)
+                   - 若l、r更新值都不等于m： l=m+1 && r=m-1，那么终结条件为最后剩1个元素： while(l<=r)
+             4.  决定返回 l或r，取决于 第2条，那个更新为m的 l 或者 r
+        */
+        while( l < r ){
+            int m = (r-l)/2 + l + 1;
+            if( nums[m] < nums[m-1] )
+                r = m-1;
+            else
+                l = m;
         }
-        
+        return l;
     }
 }
+
